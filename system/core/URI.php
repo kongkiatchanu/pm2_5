34a6ac8df9d77866fa1c34ba9ba015b2e0f4facc
signoutz <aunkaffpom@gmail.com>
2023-10-02 21:32:01 +0700

    uri

--- system/core/URI.php
+++ system/core/URI.php
@@ -1,4 +1,5 @@
 <?php
+public $config;
 /**
  * CodeIgniter
  *
@@ -50,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class CI_URI {
 
-	public $config;
+	
 	/**
 	 * List of cached URI segments
 	 *
